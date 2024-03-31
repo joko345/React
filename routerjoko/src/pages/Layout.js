b9823ab6b991af5dd20468089c88ac8387d3e774
joko345 <jokoprasetyo345.ju@gmail.com>
2024-03-31 16:46:22 +0700

    router

---
+++ routerjoko/src/pages/Layout.js
@@ -0,0 +1,25 @@
+import { Outlet, Link } from "react-router-dom";
+
+function Layout() {
+  return (
+    <>
+      <Link to="/Contact">Contact</Link>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Blogs">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/Contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet /> 
+{/* menyimpan link page lain yang sedang tidak diakses, sehingga user tidak perlu load ulang semua halaman untuk navigasi ke link page lain */}
+    </>
+  );
+}
+export default Layout;
---
+++ routerjoko/src/pages/Nopage.js
@@ -0,0 +1,4 @@
+function Nopage(){
+    return <h1>404</h1>;
+};
+export default Nopage;
