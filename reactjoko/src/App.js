1836c648fd4834279b80fccd38ac68ce2f21b9e2
joko345 <jokoprasetyo345.ju@gmail.com>
2024-03-31 12:51:27 +0700

    jsx

--- reactjoko/src/App.js
+++ reactjoko/src/App.js
@@ -26,6 +26,7 @@ function Compon() {//ekspresi javascript pada jsx
 //test
 function App() {
   return (
+
     <div className="App">{/* karena product di className app maka akan ditampilkan di tengah */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
